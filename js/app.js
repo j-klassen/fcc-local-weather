662079735d37f94fb86a37647281ffd1562a0c18
Jake Klassen <jklassendev@gmail.com>
2017-06-22 23:40:53 -0400

    https ip provider

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 
 	var apiKey = '4b77e236e4e215429aa57a7c408809c2';
 	var forecastEndpoint = 'https://api.forecast.io/forecast/' + apiKey + '/';
-	var locationEndpoint = 'http://ip-api.com/json';
+	var locationEndpoint = 'https://ipapi.co/json';
 	var locationCacheTime = 1000 * 60 * 5; // 5 minutes
 
 	// Map api response icons to classes
@@ -61,13 +61,13 @@
 			navigator.geolocation.getCurrentPosition(positionSuccess, positionError, options);
 		} else {
 			// IP based
-			$.get({ url: locationEndpoint, dataType: 'jsonp' })
+			$.get({ url: locationEndpoint, dataType: 'json' })
 			.then(function handleLocation(location) {
 				// Conform to navigator.geolocation coords property.
 				positionSuccess({
 					coords: {
-						latitude: location.lat,
-						longitude: location.lon
+						latitude: location.latitude,
+						longitude: location.longitude
 				}});
 			});
 		}
